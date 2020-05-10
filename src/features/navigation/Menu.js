852e36cf6a58c327e70090ea3b3c3414093e1309
Trayan Troev <trayan97@gmial.com>
2020-05-11 02:29:05 +0300

    Added Redirect and Link components to the navigation menu`s options.

--- src/features/navigation/Menu.js
+++ src/features/navigation/Menu.js
@@ -1,70 +1,74 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import {Image, Menu, Sticky} from 'semantic-ui-react'
 import {NavLink} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {logout, selectLogged} from "../account/accountSlice";
+import {genders} from "../account/model/User";
 
-export default class NavigationMenu extends Component {
-    state = { activeItem: 'home' }
+export default function NavigationMenu () {
+    const [activeItem, setActiveItem] = useState('home');
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    const logged = useSelector(selectLogged);
+    const dispatch = useDispatch();
 
-    render() {
-        const { activeItem } = this.state
+    return (
+        <Sticky onTop>
+            <Menu size={"large"}>
+                { logged &&
+                <React.Fragment>
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                    ><NavLink style={{color: 'black'}} to={"/fullAccountsList"}>Users</NavLink></Menu.Item>
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                    ><NavLink style={{color: 'black'}} to={"/filteredRecipes"}>Browse recipes</NavLink></Menu.Item>
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                    ><NavLink style={{color: 'black'}} to={"/fullRecipesList"}>All recipes</NavLink></Menu.Item>
+                    <Menu.Item
+                        name='messages'
+                        active={activeItem === 'messages'}
+                    ><NavLink style={{color: 'black'}} to={"/addRecipe"}>Add recipe</NavLink></Menu.Item>
+                </React.Fragment>
+                }
 
-        return (
-            <Sticky onTop>
-                <Menu size={"large"} >
-
-                    { this.props.logged &&
+                <Menu.Menu position='right'>
+                    { !logged &&
                     <React.Fragment>
                         <Menu.Item
-                            name='messages'
-                            active={activeItem === 'messages'}
-                        ><NavLink style={{color: 'black'}} to={"/fullAccountsList"}>Users</NavLink></Menu.Item>
+                            name='register'
+                            active={activeItem === 'register'}><NavLink style={{color: 'black'}} to={"/register"}>Register</NavLink></Menu.Item>
                         <Menu.Item
-                            name='messages'
-                            active={activeItem === 'messages'}
-                        ><NavLink style={{color: 'black'}} to={"/filteredRecipes"}>Short recipes</NavLink></Menu.Item>
-                        <Menu.Item
-                            name='messages'
-                            active={activeItem === 'messages'}
-                        ><NavLink style={{color: 'black'}} to={"/fullRecipesList"}>All recipes</NavLink></Menu.Item>
-                        <Menu.Item
-                            name='messages'
-                            active={activeItem === 'messages'}
-                        ><NavLink style={{color: 'black'}} to={"/addRecipe"}>Add recipe</NavLink></Menu.Item>
+                            name='login'
+                            active={activeItem === 'login'}><NavLink style={{color: 'black'}} to={"/login"}>Login</NavLink></Menu.Item>
                     </React.Fragment>
                     }
-
-                    <Menu.Menu position='right'>
-                        { !this.props.logged &&
-                        <React.Fragment>
-                            <Menu.Item
-                                name='register'
-                                active={activeItem === 'register'}><NavLink style={{color: 'black'}} to={"/register"}>Register</NavLink></Menu.Item>
-                            <Menu.Item
-                                name='login'
-                                active={activeItem === 'login'}><NavLink style={{color: 'black'}} to={"/login"}>Login</NavLink></Menu.Item>
-                        </React.Fragment>
-                        }
-                        { this.props.logged &&
-                        <React.Fragment>
-                            <Menu.Item
-                                name='logout'
-                                active={activeItem === 'logout'}><NavLink style={{color: 'black'}} to={"/login"}>Logout</NavLink></Menu.Item>
-                            <Menu.Item
-                                name='login'
-                                active={activeItem === 'login'}>
-                                <Image src='avatars/default-male.png' size={"mini"} circular />
-                            </Menu.Item>
-                        </React.Fragment>
-                        }
-                    </Menu.Menu>
-                </Menu>
-            </Sticky>
-        )
-    }
+                    { logged &&
+                    <React.Fragment>
+                        <Menu.Item
+                            name='logout'
+                            active={activeItem === 'logout'}
+                            onClick={() => dispatch(logout())}
+                        >
+                            <NavLink style={{color: 'black'}} to={"/login"}>Logout</NavLink>
+                        </Menu.Item>
+                        <Menu.Item
+                            name='login'
+                            active={activeItem === 'login'}>
+                            <Image src={logged.avatarPath.length > 0 ? logged.avatarPath :
+                                `${logged.gender === genders.MALE ? 'avatars/default-male.png' : 'avatars/default-female.png'}`}
+                                   size={"mini"} circular />
+                        </Menu.Item>
+                    </React.Fragment>
+                    }
+                </Menu.Menu>
+            </Menu>
+        </Sticky>
+    );
 }
 
 NavigationMenu.defaultProps = {
-    logged: {},
 }
